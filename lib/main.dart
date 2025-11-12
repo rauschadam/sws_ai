cf457698a3ac243514e97292aea826c010a1e3bd
Rausch Adam <rausch.adam2@gmail.com>
2025-11-12 19:33:47 +0100

    FireBase Configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
+import 'package:sws_ai/firebase_options.dart';
 import 'package:sws_ai/presentation/chat_page.dart';
 import 'package:sws_ai/presentation/chat_provider.dart';
 
 Future<void> main() async {
-  /// Get API Keys
+  /// 1. Get API Keys
   await dotenv.load(fileName: ".env");
 
+  // 2. Initalize FireBase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // 3. Run App
   runApp(
     ChangeNotifierProvider(
       create: (context) => ChatProvider(),
@@ -27,7 +33,7 @@ class MainApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue.shade700),
         useMaterial3: true,
       ),
-      home: ChatPage(),
+      home: const ChatPage(),
     );
   }
 }
